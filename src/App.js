0e86bbd9625f1950b037943322565102fb3a9296
MBeigi.Reza <mbeigi.rez@gmail.com>
2025-10-11 22:55:25 +0330

    Add Font

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ function App() {
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <h1 className="text-3xl font-bold text-blue-900">
         ToDo List App 🚀
+        بریم برای تودولیست!
       </h1>
     </div>
   );
